eb245236f49bef0d5752045b91bdcece6bb33543
Deepak <deepakom1593@gmail.com>
2025-04-17 13:53:15 +0530

    create a about page

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,13 +1,15 @@
 /** @format */
 
-import AboutUsHero from '@/components/aboutus/AboutHero';
-import Content from '@/components/aboutus/Content';
+// /** @format */
 
-export default function Pages() {
-  return (
-    <div className="flex flex-col items-center w-full">
-      <AboutUsHero />
-      <Content />
-    </div>
-  );
-}
+// import AboutUsHero from '@/components/aboutus/AboutHero';
+// import Content from '@/components/aboutus/Content';
+
+// export default function Pages() {
+//   return (
+//     <div className="flex flex-col items-center w-full">
+//       <AboutUsHero />
+//       <Content />
+//     </div>
+//   );
+// }
