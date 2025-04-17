4de430c43cc12a3df4c6cc2d01db910c2c24494f
Deepak <deepakom1593@gmail.com>
2025-04-17 13:54:10 +0530

    add functionalty in about page

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,15 +1,15 @@
 /** @format */
 
-// /** @format */
+/** @format */
 
-// import AboutUsHero from '@/components/aboutus/AboutHero';
-// import Content from '@/components/aboutus/Content';
+import AboutUsHero from '@/components/aboutus/AboutHero';
+import Content from '@/components/aboutus/Content';
 
-// export default function Pages() {
-//   return (
-//     <div className="flex flex-col items-center w-full">
-//       <AboutUsHero />
-//       <Content />
-//     </div>
-//   );
-// }
+export default function Pages() {
+  return (
+    <div className="flex flex-col items-center w-full">
+      <AboutUsHero />
+      <Content />
+    </div>
+  );
+}
